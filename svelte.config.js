e7abb248258cac153d647a6458b9822d43853598
Matthew Clarke <matthewclarke@gmail.com>
2022-06-19 17:00:55 -0700

    Code Updates
    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
-
+//import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
